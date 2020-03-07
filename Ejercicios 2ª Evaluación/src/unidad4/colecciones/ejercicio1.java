4d9be58de7f5ff1ebd565e4aade114163a2e7f32
Andrea <andreagutierrezvega@gmail.com>
2020-03-07 11:59:48 +0100

    ejercicio 1 de la unidad 4 (colecciones) resuelto

---
+++ Ejercicios 2ª Evaluación/src/unidad4/colecciones/ejercicio1.java
@@ -0,0 +1,28 @@
+package unidad4.colecciones;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class ejercicio1 {
+	
+	public static void main(String[] args) {
+	
+		List<String> nombres = new ArrayList<>();
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Introduzca un número de veces");
+		int num = scan.nextInt();
+		
+		for (int i=0; i<num; i++) {
+			
+			String nombre = scan.next();
+					if(!nombres.contains(nombre))
+						nombres.add(nombre);
+		}
+		scan.close();
+		System.out.println(nombres);
+	}
+
+}

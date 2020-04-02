b86886534835ae17c49319a4fa3a52cabf1849bb
Andrea <andreagutierrezvega@gmail.com>
2020-04-02 20:19:09 +0200

    Ejercicio de repaso 2ª evaluación - Automóviles (incompleto)

---
+++ Ejercicios 2ª Evaluación/src/repaso2ev/Automovil.java
@@ -0,0 +1,88 @@
+package repaso2ev;
+
+public class Automovil {
+
+	private String modelo;
+	private double capacidadDeposito;
+	private double cantidadInicialDeposito;
+	private double consumo;
+	private double kmRecorridos;
+	private double combustibleConsumido;
+	
+	public Automovil (String modelo, double capacidadDeposito, double cantidadInicialDeposito, double consumo) {
+		
+		this.modelo = modelo;
+		this.capacidadDeposito = capacidadDeposito;
+		this.cantidadInicialDeposito = cantidadInicialDeposito;
+		this.consumo = consumo;
+		this.kmRecorridos = 0;
+		this.combustibleConsumido = 0;
+	}
+	
+	public Automovil (String modelo, double capacidadDeposito, double consumo) {
+		
+		this.modelo = modelo;
+		this.capacidadDeposito = capacidadDeposito;
+		this.consumo = consumo;
+		this.cantidadInicialDeposito = capacidadDeposito;
+		this.kmRecorridos = 0;
+		this.combustibleConsumido = 0;
+	}
+	
+	public double llenar_deposito() {
+		
+		cantidadInicialDeposito = capacidadDeposito;
+		return 0;
+	}
+	
+	public double llenar_deposito (int litros) {
+		
+		if(litros+cantidadInicialDeposito > capacidadDeposito)
+			
+			return litros+cantidadInicialDeposito-capacidadDeposito;
+		else
+			
+			return 0;
+	}
+	
+	public double desplazarse (double km) {
+		
+		if (cantidadInicialDeposito > consumo*km) {
+			//se puede
+			this.kmRecorridos = this.kmRecorridos + km;
+			this.combustibleConsumido = this.combustibleConsumido + consumo*km;
+			return cantidadInicialDeposito - (consumo*km);
+		}else {
+			//no se puede
+			return -1;
+		}
+	}
+	
+	public String get_modelo() {
+		
+		return modelo;
+	}
+	
+	public double get_consumo() {
+		
+		return consumo;
+	}
+	public double get_capacidadDeposito() {
+		
+		return capacidadDeposito;
+	}
+	public double get_kmRecorridos() {
+		
+		return kmRecorridos;
+	}
+	public double get_combustibleConsumido() {
+		
+		return combustibleConsumido;
+	}
+	
+	@Override
+	public String toString() {
+		return this.modelo + " " + this.capacidadDeposito + " " + this.cantidadInicialDeposito+ " " + this.consumo;
+	}
+
+}
---
+++ Ejercicios 2ª Evaluación/src/repaso2ev/PruebaAutomovil.java
@@ -0,0 +1,107 @@
+package repaso2ev;
+
+import repaso2ev.Automovil;
+
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+
+public class PruebaAutomovil {
+	
+	
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner (System.in);
+		int nAutos = Integer.parseInt(scan.nextLine());
+		
+		//definimos el mapa de automoviles
+		Map<String, Automovil> automoviles = new TreeMap<>();
+		//ejemplo (1,auto1);
+		
+		
+		//añadimos al mapa las n entradas siguientes
+		
+		for (int i=0; i<nAutos; i++) {
+			
+			String entradaArr [] = scan.nextLine().split(" ");
+			
+			String modelo = entradaArr[0];
+			double capacidadDep = Double.parseDouble(entradaArr[1]);
+			double litrosHay = Double.parseDouble(entradaArr[2]);
+			double consumo = Double.parseDouble(entradaArr[3]);
+			
+			//creamos el objeto auto
+			Automovil auto = new Automovil(modelo, capacidadDep, litrosHay, consumo);
+			
+			//añadimos el objeto al mapa
+			automoviles.put(modelo, auto);
+			
+		}
+		boolean repetir = true;
+		while (repetir = true) {
+			
+			String accion = scan.nextLine().trim();
+			String accionArr [] = accion.split(" ");
+			
+			
+			if(accionArr.length != 1) {
+				
+				
+				accion = accionArr[0];
+				String modelo = accionArr[1];
+				double kilometros = Double.parseDouble(accionArr[2]);
+				
+				if(accion.equals("desplazar")) {
+					
+					for (Entry<String, Automovil> e: automoviles.entrySet()) {
+						
+						if(e.getKey().equals(modelo)) {
+							
+							Automovil auto = e.getValue();
+							double cantidadDeposito = auto.desplazarse(kilometros);
+							
+							if(cantidadDeposito == -1) {
+								System.out.println("Combustible insuficiente para este desplazamiento");
+							}else {
+								
+								double consumo = auto.get_consumo();
+								double consumido = consumo*kilometros;
+								System.out.println(modelo + " " + cantidadDeposito + " " + consumido);
+								
+								//añadimos al objeto auto 
+								//sustituimos la cantidad inicial del depósito
+								double capacidadDeposito = auto.get_capacidadDeposito();
+								Automovil autoSustituir = new Automovil(modelo, capacidadDeposito ,cantidadDeposito, consumo);
+								automoviles.put(modelo,autoSustituir);
+								
+							}
+							
+						}
+					}
+					
+				}else {
+					System.out.println("nada de mmento");
+				}
+				
+				repetir = true;	
+				//System.out.println(automoviles);
+			}else {
+				//System.out.println(automoviles);
+				for (String e: automoviles.keySet()) {
+					
+					String modelo = automoviles.get(e).get_modelo();
+					double combCons = automoviles.get(e).get_combustibleConsumido();
+					double kmRec = automoviles.get(e).get_kmRecorridos();
+					System.out.println(modelo + " " + kmRec + " " + combCons);
+				}
+				repetir = false;
+			}
+		}
+		System.out.println("");
+		scan.close();
+	}
+
+}
